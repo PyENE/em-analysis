2c7bc6394322ad0c3ff72abc5f7de8922129f898
brice-olivier <briceolivier1409@gmail.com>
2018-10-16 20:10:43 +0200

    rm openalea dependencies

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -n "last test run:" > ../test/test_log.txt
-date >> ../test/test_log.txt
-py.test -q -s ../test/test_*.py > /tmp/test_output
-cat /tmp/test_output | tail -1 >> ../test/test_log.txt
-cat /tmp/test_output >> ../test/test_log.txt
+echo -n "last test run:" > ../tests/test_log.txt
+date >> ../tests/test_log.txt
+py.test -q -s ../tests/test_*.py > /tmp/test_output
+cat /tmp/test_output | tail -1 >> ../tests/test_log.txt
+cat /tmp/test_output >> ../tests/test_log.txt
 rm /tmp/test_output
---
+++ ema/config.py
@@ -0,0 +1,18 @@
+import os
+import seaborn as sns
+
+PROJECT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..' + os.sep + '..' + os.sep)
+SHARE_PATH = os.path.join(PROJECT_PATH, 'share')
+DATA_PATH = os.path.join(SHARE_PATH, 'data')
+PNG_TEXTS_PATH = os.path.join(DATA_PATH, "Materiel")
+MODELS_PATH = os.path.join(SHARE_PATH, 'models')
+GRAPHICS_PATH = os.path.join(SHARE_PATH, 'graphics')
+REPORTS_PATH = os.path.join(SHARE_PATH, 'reports')
+SRC_PATH = os.path.join(PROJECT_PATH, 'src')
+ANALYSIS_PATH = os.path.join(PROJECT_PATH, 'notebooks')
+TEST_PATH = os.path.join(PROJECT_PATH, 'test')
+FIXATION_PATH = os.path.join(PROJECT_PATH, '..' + os.sep + 'fixation-word-assignation')
+PLOTS_PATH = os.path.join(PROJECT_PATH, '..' + os.sep + 'plots')
+FASTTEXT_PATH = os.path.join(PROJECT_PATH, '..', 'fasttext')
+
+COLOR_PALETTE = sns.color_palette('dark', 10)
